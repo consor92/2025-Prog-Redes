84b9002913d39a93a568161330c52b14bbe99dac
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2025-04-09 12:45:58 -0300

    colecciones

--- introducionJava/src/main/java/CadenaDeCaracteres/main.java
+++ introducionJava/src/main/java/CadenaDeCaracteres/main.java
@@ -1,7 +1,20 @@
 package CadenaDeCaracteres;
 
+import java.io.PrintStream;
+import java.util.StringTokenizer;
+
 public class main {
 
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_MAGENTA = "\u0033[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_RESET = "\u001B[0m";	
+	
 	public static void main(String[] args) {
 		
 		String palabra = "LinKevin";
@@ -28,10 +41,29 @@ public class main {
 		// ['L','i','n','K','e','v','i','n']
 		palabra.split("K"); // vec[] => { "Lin" , "evin" } Lin evin
 		palabra.substring( 2 , 4 ); //LinKevin  -> nKev 
-		palabra.
+		
 		
 		//conversion de Enteros a texto y viceversa
 		//Tokenized
+		// String = 75+9+63=10
+		StringTokenizer st = new StringTokenizer( "75+9+36=10" , "\\+" );
+		PrintStream ps = new PrintStream(System.out);
+		
+		//  ANSIcolor + texto + ANSIreset
+		ps.println( ANSI_GREEN.concat( String.valueOf(st.countTokens() ).concat(ANSI_RESET) )  ); // 3
+		
+		while( st.hasMoreTokens() )
+		{
+			ps.println( st.nextToken() );   // el texto en si como STRING
+		}
+		
+		//ps.println( st.nextToken() ); 
+		//ps.println( st.nextToken() ); 
+		//ps.println( st.nextToken() ); 
+		//ps.println( st.nextElement() ); // objeto representato con STRING
+		
+		int num = Integer.parseInt("9");
+		
 	}
 
 }
--- introducionJava/src/main/java/Colecciones/main.java
+++ introducionJava/src/main/java/Colecciones/main.java
@@ -1,25 +1,24 @@
 package Colecciones;
 
-import java.io.StreamCorruptedException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Vector;
 
 public class main {
 
 	public static void main(String[] args) {
-
-		int[] vec = {} ;
-		List listita = new ArrayList<>();
-		ArrayList<Integer> arr = new ArrayList<>();
-		LinkedList<E> lik = new LinkedList<>();
-		Map<K, V> map = new HashMap<>();
-		Map<K, V> set = new HashSet<>();
-		Vector<E> vect = new Vector<>();
+		//vector
+		int[] vector = new int[5];
+		
+		//Crear una Lista
+		ArrayList<String> nombres = new ArrayList<>();
+		ArrayList comidas = new ArrayList<>();
+		List<Integer> numeros = new ArrayList<>();
+		
+		
+		LinkedList<Integer> otraLista = new LinkedList<>();
+		
+		List<String> productos;
 		
 	}
 
