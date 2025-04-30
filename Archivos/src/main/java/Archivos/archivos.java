e58a46fc356d9451063ff704510e5216e477c351
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2025-04-30 12:49:13 -0300

    Crear files con clase PrintStream

---
+++ Archivos/src/main/java/Archivos/archivos.java
@@ -0,0 +1,51 @@
+package Archivos;
+
+import java.io.File;
+
+public class archivos {
+ 
+
+/**
+ * ESTE TEXTO NO TIENE NINGUNA ETIQUETA DE IDENTIFICACION. ESTO NO VA A
+ * AAPARECER :( Tambien se puede agregar referencias a class o methodos o
+ * atributos con la instruccion: {
+ *
+ * @ por ejemplo: {@code <html></html>} o usar {@link String}
+ *
+ * @param f Este metodo recibe un archivo.
+ * @see FlujoDeDatos.File.
+ * @since v1.0
+ */
+public void rutaFiles(File f)
+{
+}
+
+public void crearFileConPrintStreamEasy(File f) {
+}
+
+public void crearFileConPrinter(File f) {
+}
+
+/**
+*
+* @param f
+*/
+public void crearFileConBuffer(File f) {
+}
+
+
+/**
+ * Descripcion
+ *
+ * @param f un archivo al leer
+ * @return Todo el texto leido.
+ * @throws FileNotFoundException
+ * @throws IOException
+ */
+public String LeerFileConBuffer(File f) throws FileNotFoundException, IOException {
+}
+
+public void leerFileCaracterAcaractet(File f) {
+}
+
+}
